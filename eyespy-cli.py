ea21b5b9b49ba05c56bb42ca162b71acaade081c
Phillip Tennen <phillip.ennen@gmail.com>
2018-02-06 00:03:06 -0500

    fix bug in eye bounds tracking where right eye history looked to left eye for history popping

--- eyespy-cli.py
+++ eyespy-cli.py
@@ -10,15 +10,16 @@ class EyeSide(Enum):
 
 
 class Detector(object):
-    def __init__(self, w, h):
-        self.w = w
-        self.h = h
+    _CACHE_SIZE = 5
 
+    def __init__(self):
         self.face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
         self.eye_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
         self.last_left_eyes = []
+        self.last_left_pupil_keypoints = []
         self.last_right_eyes = []
+        self.last_right_pupil_keypoints = []
 
     def process_frame(self, frame):
         # type: (np.ndarray) -> np.ndarray
@@ -37,20 +38,61 @@ class Detector(object):
 
             for eye_side in EyeSide:
                 tracked_eye_bounds = self.get_tracked_eye(eye_side)
-                if not tracked_eye_bounds:
+                if not tracked_eye_bounds or (tracked_eye_bounds[2] == 0 and tracked_eye_bounds[3] == 0):
                     continue
-                print('Tracked {}: {}'.format(eye_side.name, tracked_eye_bounds))
+                # print('Tracked {}: {}'.format(eye_side.name, tracked_eye_bounds))
                 ex, ey, ew, eh = tracked_eye_bounds
                 cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+                # find pupil and draw
+                eye_frame = roi_color[ey:ey+eh, ex:ex+ew]
+                eye_gray = roi_gray[ey:ey+eh, ex:ex+ew]
+
+                pupil_feature = self.detect_pupil_in_eye_frame(eye_gray)
+                self.cache_pupil_feature(eye_side, pupil_feature)
+
+                # draw time-averaged pupil
+                pupil_feature = self.get_tracked_pupil_keypoint(eye_side)
+
+                # Draw detected blobs as red circles.
+                # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+                drawn_eye_frame = cv2.drawKeypoints(eye_frame, [pupil_feature], np.array([]), (0, 0, 255),
+                                                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+                np.copyto(eye_frame, drawn_eye_frame)
         return frame
+
+    def detect_pupil_in_eye_frame(self, eye_gray):
+        # type: (np.ndarray) -> Optional[cv2.KeyPoint]
+        # filter out all pixels that are lighter than 60
+        _, pupil_pixels = cv2.threshold(eye_gray, 60, 255, cv2.THRESH_BINARY_INV)
+
+        # feature detector
+        # MSER detects pupil well
+        blob_detector = cv2.MSER_create(_max_area=2048, _min_area=240)
+        keypoints = blob_detector.detect(pupil_pixels)
+
+        # trivially select the keypoint with the lowest Y, if multiple are detected
+        # this metric was selected through experiment, and noticing that often features would be detected in
+        # the eyebrow. This selection tries to filter out eyebrow features.
+        pupil_keypoint = None
+        if len(keypoints):
+            pupil_keypoint = keypoints[0]
+            for point in keypoints:
+                # compare Y's
+                if point.pt[1] > pupil_keypoint.pt[1]:
+                    pupil_keypoint = point
+        # turn grayscale back to RGB, so we can copy it to the framebuffer
+        # pupil_pixels = cv2.cvtColor(pupil_pixels, cv2.COLOR_GRAY2RGB)
+        return pupil_keypoint
 
     def process_eye_bounding_box(self, face_frame, bounding_box):
         # type: (np.ndarray, Tuple[int, int, int, int]) -> np.ndarray
         ex, ey, ew, eh = bounding_box
 
         # draw raw bounds
-        raw_bounds_color = (0, 0, 255)
-        cv2.rectangle(face_frame, (ex, ey), (ex + ew, ey + eh), raw_bounds_color, 2)
+        #raw_bounds_color = (0, 0, 255)
+        #cv2.rectangle(face_frame, (ex, ey), (ex + ew, ey + eh), raw_bounds_color, 2)
 
         # find center of eye rect
         eye_midx = ex + (ew / 2)
@@ -98,8 +140,10 @@ class Detector(object):
             array = self.last_left_eyes
         else:
             array = self.last_right_eyes
+        if len(array) > Detector._CACHE_SIZE:
+            # pop the oldest frame
+            array.pop(0)
         array.append(bounds)
-        if len(self.last_left_eyes) > 5:
             # pop the oldest frame
             array.pop(0)
 
@@ -127,13 +171,13 @@ class CameraStream(object):
         self.capture.release()
         return None
 
-detector = Detector(1280, 780)
+detector = Detector()
 with CameraStream() as stream:
-    while cv2.waitKey(30) == -1:
+    while cv2.waitKey(25) != 27:
         # grab a camera frame
         frame = stream.get_frame()
         if frame is None:
             continue
         # process it
         frame = detector.process_frame(frame)
-        cv2.imshow("Pupil detection!", frame)
+        cv2.imshow('Pupil tracking', frame)
